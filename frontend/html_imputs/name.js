c40ec4ca37a7fa888a652efc27cc73daeb239839
FrirexD <115400655+FrirexD@users.noreply.github.com>
2024-06-02 19:13:49 +0200

    front added

--- frontend/html_imputs/name.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('chooseNameLink').addEventListener('click', function(event) {
-    event.preventDefault();
-    var playerNameInput = document.getElementById('playerName');
-    playerNameInput.style.display = 'block';
-  
-  });
-  
